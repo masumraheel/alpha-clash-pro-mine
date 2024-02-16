567724e5c8d014873069cb95f92634e8c5799f7a
masumraheel <masumhossain017252@gmail.com>
2024-02-17 02:40:39 +0600

    added utility changes

--- script/alpha_clash.js
+++ script/alpha_clash.js
@@ -12,15 +12,6 @@
 
 function play(){
     hideElementById("home-screen")
-    showElementById("")
+    showElementById("play-ground")
 }
 
-function hideElementById(elementId){
-    const element=document.getElementById(elementId);
-    element.classList.add('hidden');
-}
-
-function showElementById(){
-    const element= document.getElementById(elementId);
-    element.classList.remove("hidden");
-}
